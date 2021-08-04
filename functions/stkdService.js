fec4b8d6fd053354f3fd7027dd40e9652819159e
Sandy Corsillo <the_dusky@hey.com>
2021-08-03 22:02:41 -0400

    add dynamic time

--- functions/stkdService.js
+++ functions/stkdService.js
@@ -3,8 +3,9 @@ const { CosmWasmClient } = require('secretjs');
 const getSTKDInfo = async () => {
   try {
     const client = new CosmWasmClient('http://api.holodeck.stakeordie.com'); 
-    const contractAddress = 'secret1mtg8tuqsfvk9ma36tmtz5mc7pae73j0hsc6jla'; 
-    const {staking_info} = await client.queryContractSmart(contractAddress,{"staking_info":{"time":1623974345}})
+    const contractAddress = 'secret1mtg8tuqsfvk9ma36tmtz5mc7pae73j0hsc6jla';
+    const secondsSinceEpoch = Math.round(Date.now() / 1000)
+    const {staking_info} = await client.queryContractSmart(contractAddress,{"staking_info":{"time":secondsSinceEpoch}})
 
     return staking_info;
   } catch (error) {
