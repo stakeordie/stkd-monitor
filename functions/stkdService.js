c3bd171d51162949193451a22615e11ff6bed006
Sandy Corsillo <the_dusky@hey.com>
2021-08-03 21:44:02 -0400

    updated contract address

--- functions/stkdService.js
+++ functions/stkdService.js
@@ -3,7 +3,7 @@ const { CosmWasmClient } = require('secretjs');
 const getSTKDInfo = async () => {
   try {
     const client = new CosmWasmClient('http://api.holodeck.stakeordie.com'); 
-    const contractAddress = 'secret1d54dvtpmptzk4snwc405r263ahq0nqr5ajndt5'; 
+    const contractAddress = 'secret1mtg8tuqsfvk9ma36tmtz5mc7pae73j0hsc6jla'; 
     const {staking_info} = await client.queryContractSmart(contractAddress,{"staking_info":{"time":1623974345}})
 
     return staking_info;
