442e025317fec4ab779aa4465d66fde22ae0ca1b
Sandy Corsillo <the_dusky@hey.com>
2021-08-03 23:22:11 -0400

    mainnet

--- functions/stkdService.js
+++ functions/stkdService.js
@@ -2,8 +2,8 @@ const { CosmWasmClient } = require('secretjs');
 
 const getSTKDInfo = async () => {
   try {
-    const client = new CosmWasmClient('http://api.holodeck.stakeordie.com'); 
-    const contractAddress = 'secret1mtg8tuqsfvk9ma36tmtz5mc7pae73j0hsc6jla';
+    const client = new CosmWasmClient('http://api.stakeordie.com'); 
+    const contractAddress = 'secret18cgnku4dcd8scn56t3ru63ludh3lm0svemul8v';
     const secondsSinceEpoch = Math.round(Date.now() / 1000)
     const {staking_info} = await client.queryContractSmart(contractAddress,{"staking_info":{"time":secondsSinceEpoch}})
 
